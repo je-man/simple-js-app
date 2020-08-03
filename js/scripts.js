f1b19c1ce7acc8d878213650463d6626827eb913
je-man <61508086+je-man@users.noreply.github.com>
2020-08-03 10:43:04 -0400

    Task 1.1

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var myName = "Bob";
+document.write(myName);
+myName = "John Doe";
+document.write(myName);
+
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+//example 2
+
+var size = 100;
+var doubleSize = size * 2;
+document.write(doubleSize);
+
+//example 3
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = doubleSize * 2 - size / 2;
+document.write(minSize);
