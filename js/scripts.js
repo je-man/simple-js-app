29f2584e50ba80b00f2b7fb290dc3b79827a8a54
je-man <61508086+je-man@users.noreply.github.com>
2020-08-04 12:07:12 -0400

    created an array for Task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,37 @@
-alert("Hello world");
-
-var favoriteFood = "French";
-document.write(favoriteFood);
+pokemonList = [
+  {
+    pokeName: "Blastoise",
+    pokeHeight: 1.6,
+    pokeCategory: "Shellfish",
+    pokeAbilities: "Torrent",
+    pokeType: "Water",
+  },
+  {
+    pokeName: "Butterfree",
+    pokeHeight: 1.1,
+    pokeCategory: "Butterfly",
+    pokeAbilities: "Compound Eyes",
+    pokeType: "Bug",
+  },
+  {
+    pokeName: "Pidgey",
+    pokeHeight: 0.3,
+    pokeCategory: "Tiny Bird",
+    pokeAbilities: "Keen Eye",
+    pokeType: "Flying",
+  },
+  {
+    pokeName: "Pikachu",
+    pokeHeight: 0.4,
+    pokeCategory: "Mouse",
+    pokeAbilities: "Static",
+    pokeType: "Electric",
+  },
+  {
+    pokeName: "Vulpix",
+    pokeHeight: 0.6,
+    pokeCategory: "Fox",
+    pokeAbilities: "Flash Fire",
+    pokeType: "Fire",
+  },
+];
