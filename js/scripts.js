f4819fbbe049552412bc75289550140d34fa177f
je-man <61508086+je-man@users.noreply.github.com>
2020-08-05 13:05:53 -0400

    created a loop iterates over each item in pokemonList for Task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,37 @@ pokemonList = [
     pokeType: "Fire",
   },
 ];
+
+//create loop that iterates over each item in pokemonList
+
+// for (var i = 0; i < pokemonList.length; i++) {
+//   document.write(
+//     "Pokemon Name: " +
+//       "<strong>" +
+//       pokemonList[i].pokeName +
+//       "</strong>" +
+//       " ( height: " +
+//       pokemonList[i].pokeHeight +
+//       "m)" +
+//       "<br>" +
+//       "<br>"
+//   );
+// }
+
+for (var i = 0; i < pokemonList.length; i++) {
+  document.write(
+    "Pokemon Name: " +
+      "<strong>" +
+      pokemonList[i].pokeName +
+      "</strong>" +
+      "<br>" +
+      " Height: " +
+      pokemonList[i].pokeHeight +
+      "m"
+  );
+  if (pokemonList[i].pokeHeight >= 0.6) {
+    document.write(" - " + "Wow that's big!" + "<br>" + "<br>");
+  } else {
+    document.write(" - " + "Wow that's cute!" + "<br>" + "<br>");
+  }
+}
