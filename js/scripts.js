2719c6480ce0d9907d8e66e562f70bcec3d9165d
je-man <61508086+je-man@users.noreply.github.com>
2020-08-07 10:09:27 -0400

    change for loop to forEachloop

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ pokemonList = [
   },
 ];
 
-//create loop that iterates over each item in pokemonList
+//create for loop that iterates over each item in pokemonList
 
 // for (var i = 0; i < pokemonList.length; i++) {
 //   document.write(
@@ -44,28 +44,34 @@ pokemonList = [
 //       "<strong>" +
 //       pokemonList[i].pokeName +
 //       "</strong>" +
-//       " ( height: " +
-//       pokemonList[i].pokeHeight +
-//       "m)" +
 //       "<br>" +
-//       "<br>"
+//       " Height: " +
+//       pokemonList[i].pokeHeight +
+//       "m"
 //   );
+//   if (pokemonList[i].pokeHeight >= 0.6) {
+//     document.write(" - " + "Wow that's big!" + "<br>" + "<br>");
+//   } else {
+//     document.write(" - " + "Wow that's cute!" + "<br>" + "<br>");
+//   }
 // }
 
-for (var i = 0; i < pokemonList.length; i++) {
+//create forEach loop that iterates over each item in pokemonList
+
+pokemonList.forEach(function (pokemonChecklist) {
   document.write(
     "Pokemon Name: " +
       "<strong>" +
-      pokemonList[i].pokeName +
+      pokemonChecklist.pokeName +
       "</strong>" +
       "<br>" +
       " Height: " +
-      pokemonList[i].pokeHeight +
+      pokemonChecklist.pokeHeight +
       "m"
   );
-  if (pokemonList[i].pokeHeight >= 0.6) {
+  if (pokemonChecklist.pokeHeight >= 0.6) {
     document.write(" - " + "Wow that's big!" + "<br>" + "<br>");
   } else {
     document.write(" - " + "Wow that's cute!" + "<br>" + "<br>");
   }
-}
+});
