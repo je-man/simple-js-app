e1aea8bcb2032ca74156dbfbfd268a54cce1474e
je-man <61508086+je-man@users.noreply.github.com>
2020-09-24 12:03:43 -0400

    semi-final pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -35,7 +35,7 @@ var pokemonRepository = (function () {
     pokemonRepository.loadDetails(pokemon).then(function () {
       let row = $(".row");
       let card = $(
-        '<div class="card mt-5" style="width: 18rem; margin:10px;"></div>'
+        '<div class="card mt-5" style="width: 18rem; margin:13px;"></div>'
       );
       let image = $('<img class="card-img-top" alt="...">');
       let title = $('<h5 class="card-title">' + pokemon.name + "</h5>");
@@ -75,7 +75,10 @@ var pokemonRepository = (function () {
       .then(function (details) {
         // Now we add the details to the item
         item.imageUrlFront = details.sprites.other.dream_world.front_default;
-        item.imageUrlBack = details.sprites.back_default;
+        item.imageUrlBack =
+          details.sprites.versions["generation-v"][
+            "black-white"
+          ].animated.back_default;
         item.height = details.height;
         // item.types = details.types;
         item.types = [];
@@ -96,15 +99,15 @@ var pokemonRepository = (function () {
     let modalTitle = $(".modal-title");
     modalTitle.empty();
     let modalHeader = $(".modal-header");
-    let pokemonName = $('<h1 style="color: whites;">' + item.name + "</h1>");
-    let modalBody = $(".modal-body")
+    let pokemonName = $('<h1 style="color: white;">' + item.name + "</h1>");
+    let modalBody = $(".modal-body");
     modalBody.empty();
     let imageFront = $(
       '<img class="modal-img" alt="..." style="width: 50%; padding: 30px;">'
     );
     imageFront.attr("src", item.imageUrlFront);
     let imageBack = $(
-      '<img class="modal-img" alt="..." style="width: 50%; padding: 30px;">'
+      '<img class="modal-img" alt="..." style="width: 35%; padding: 30px;">'
     );
     imageBack.attr("src", item.imageUrlBack);
     let modalProfile = $(
@@ -152,8 +155,8 @@ var pokemonRepository = (function () {
       $(".modal-header").css("background-color", "rgb(232, 120, 144)");
     } else if (item.types.includes("ghost")) {
       $(".modal-header").css("background-color", "rgb(100, 78, 136)");
-    } else if(item.types.includes("normal")) {
-      $('.modal-header').css("background-color"), "rgb(156, 156, 99)");
+    } else if (item.types.includes("normal")) {
+      $(".modal-header").css("background-color", "rgb(156, 156, 99)");
     }
   }
 
@@ -181,3 +184,22 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+function search() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  // li = ul.getElementsByTagName("");
+  li = ul.querySelectorAll(".card");
+  for (i = 0; i < li.length; i++) {
+    // a = li[i].getElementsByTagName("a")[0];
+    a = li[i].querySelector(".card-body").querySelector(".card-title")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
